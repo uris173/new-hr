4bbf9d4a0c6827832e5daba6fde64291d51df38e
unknown <uriskatpks171@gmail.com>
2025-03-17 12:11:57 +0500

    17.03.2025

--- utils/workers/door-worker.js
+++ utils/workers/door-worker.js
@@ -3,17 +3,49 @@ import { EventModel } from "../../models/data/event.js";
 import { UserModel } from "../../models/data/user.js";
 
 const saveEventDoor = async (data) => {
-  let doorIds = Object.keys(data);
+  try {
+    let eventPromises = [];
 
-  await Promise.all(doorIds.map(async (doorId) => {
-    await Promise.all(data[doorId].map(async (event) => {
-      let user = await UserModel.findOne({ employeeNoString: event.employeeNoString }, "_id");
-      
-    }));
+    Object.entries(data).forEach(([doorId, events]) => {
+      events.forEach((event) => {
+        eventPromises.push(
+          (async () => {
+            const user = await UserModel.findOne({ employeeNoString: event.employeeNoString }, "_id");
 
-  }));
+            if (!user) return null;
+
+            return {
+              type: event.type,
+              time: event.time,
+              user: user._id,
+              door: doorId,
+              branch: event.branch,
+              employeeNoString: event.employeeNoString,
+              serialNo: event.serialNo,
+              pictureURL: event.pictureURL,
+            };
+          })()
+        );
+      });
+    });
+
+    const eventDocs = (await Promise.all(eventPromises)).filter(Boolean);
+
+    if (eventDocs.length > 0)
+      await EventModel.insertMany(eventDocs);
+
+    return "success";
+  } catch (err) {
+    console.error("Door-worker error:", err);
+    throw err;
+  }
 };
 
-parentPort.on("message", (data) => {
-
+parentPort.on("message", async (data) => {
+  try {
+    const result = await saveEventDoor(data);
+    parentPort.postMessage(result);
+  } catch (err) {
+    parentPort.postMessage({ error: err.message });
+  }
 });
